9c4aada8e680ab2bd6e3a707fe7ab510536e36b0
blue-collar-programmer <89601260+blue-collar-programmer@users.noreply.github.com>
2022-01-17 13:25:33 -0800

    finished javaScript code for tab-selector. Made changes to html- removed <br> elements from <p> elements in the content-body div- Also made changes to the css- 1. added a flex display to content-body class and added media query break points for different size devices

--- Dynamic-Tab-Selector/tab-selector.js
+++ Dynamic-Tab-Selector/tab-selector.js
@@ -1,19 +1,32 @@
-// create a function that swtiches the "show-active-content" when a new tab is selected
+var contents = document.getElementsByClassName('content-body');
+var tabs =     document.getElementsByClassName('tab');
+
+// create a function that swtiches the "active-content" when a new tab is selected
+function showActiveContent(clickedTab){
+    
     // should remove "show-active-content" class from current div elem
-    // assign "show-active-content" class to the div id that matches name of "active-tab" class text-content
-    // remove active-tab class
-    // reassign active-tab class to the tab that is clicked (i.e the currentTarget)
-
-var contents = document.getElementsByClassName('content-body');    
-    function showActiveContent(){
-
-        for(var content of contents ){
-        console.log(content, "test1");
+    for(var content of contents ){
         content.classList.remove('active-content');
-        }
     }
-    showActiveContent()
-//create a loop that deselects the highlighted tab or "active-tab"
+    // reassign "active-content" class to the div id that matches name of "active-tab" class text-content
+     document.getElementById(clickedTab.textContent).classList.add('active-content');
+    
+    // remove active-tab class
+    for(var tab of tabs){
+        console.log('test-2', tab)
+        tab.classList.remove('active-tab');
+    }
+    clickedTab.classList.add('active-tab');
+    // reassign active-tab class to the tab that is clicked (i.e the currentTarget)
+    }
+    
+    //create a loop that deselects the highlighted tab or "active-tab"
+    for(var tab of tabs){
+        tab.classList.remove('active-tab');
+    tab.addEventListener('click', function(e){
+        showActiveContent(e.currentTarget);
+    });
+}
     // within this same loop we want to add a new eventListener
      // place our showActive function in the body and call it 
 
